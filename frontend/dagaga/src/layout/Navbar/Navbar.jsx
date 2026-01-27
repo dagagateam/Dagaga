b72ad834a4e3696a25d7d76a3f2f782058eb24fd
ohyu628 <hhxx0083@gmail.com>
2026-01-27 16:39:22 +0900

    Fix: develop 브랜치 머지

--- frontend/dagaga/src/layout/Navbar/Navbar.jsx
+++ frontend/dagaga/src/layout/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import { Navbar as BootstrapNavbar, Nav, Container, Dropdown } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import logo from "../../assets/icons/logo.png";
 import alarm_bell from "../../assets/icons/alarm_bell.png";
 import bell from "../../assets/icons/bell.png";
@@ -17,7 +16,7 @@ const Navbar = () => {
 
         {/* 중앙 메뉴 */}
         <Nav className="position-absolute start-50 translate-middle-x d-none d-md-flex" style={{ gap: "100px" }}>
-          <Nav.Link as={Link} to="/scenario-select" className="fw-medium text-dark p-0">
+          <Nav.Link href="#" className="fw-medium text-dark p-0">
             학습
           </Nav.Link>
           <Dropdown as={Nav.Item}>
--- frontend/dagaga/src/router/router.jsx
+++ frontend/dagaga/src/router/router.jsx
@@ -1,18 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ScenarioSelect from "../pages/Scenario-Select/Scenario-select";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "scenario-select",
-        element: <ScenarioSelect />,
-      },
-    ],
-  },
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "scenario-select",
+                element: <ScenarioSelect />,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
 ]);
 
 export default router;
