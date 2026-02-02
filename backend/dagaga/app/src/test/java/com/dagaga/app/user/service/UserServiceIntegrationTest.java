0fdb08d5e789d5737bba891cc465f15440882dec
Arin Kim <arin.kim0801@gmail.com>
2026-02-02 11:12:07 +0900

    Fix: User 통합 테스트, user 서비스 테스트, 프로그램 테스트 수정

--- backend/dagaga/app/src/test/java/com/dagaga/app/user/service/UserServiceIntegrationTest.java
+++ backend/dagaga/app/src/test/java/com/dagaga/app/user/service/UserServiceIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,125 +19,128 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @Transactional
 class UserServiceIntegrationTest {
 
-    @Autowired
-    private UserService userService;
+        @Autowired
+        private UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
+        @Autowired
+        private UserRepository userRepository;
 
-    @Test
-    @DisplayName("회원가입 성공 - 닉네임이 있을 때")
-    void register_success_with_nickname() {
-        // given
-        UserRegisterDto dto = UserRegisterDto.builder()
-                .email("test-integration-app@gmail.com")
-                .password("Password123*")
-                .nickname("my-nickname")
-                .viewLangCode("ko")
-                .nativeLangCode("en")
-                .build();
+        @Autowired
+        private PasswordEncoder passwordEncoder;
 
-        // when
-        Integer userId = userService.register(dto);
+        @Test
+        @DisplayName("회원가입 성공 - 닉네임이 있을 때")
+        void register_success_with_nickname() {
+                // given
+                UserRegisterDto dto = UserRegisterDto.builder()
+                                .email("test-integration-app@gmail.com")
+                                .password("Password123*")
+                                .nickname("my-nickname")
+                                .viewLangCode("ko")
+                                .nativeLangCode("en")
+                                .build();
 
-        // then
-        User savedUser = userRepository.findById(userId).orElseThrow();
-        assertThat(savedUser.getEmail()).isEqualTo("test-integration-app@gmail.com");
-        assertThat(savedUser.getNickname()).isEqualTo("my-nickname");
-    }
+                // when
+                User user = userService.register(dto);
 
-    @Test
-    @DisplayName("회원가입 성공 - 닉네임이 비어있어 자동 생성될 때")
-    void register_success_auto_nickname() {
-        // given
-        UserRegisterDto dto = UserRegisterDto.builder()
-                .email("auto-nick-app@naver.com")
-                .password("Password123+")
-                .nickname("")
-                .viewLangCode("ko")
-                .nativeLangCode("en")
-                .build();
+                // then
+                User savedUser = userRepository.findById(user.getUserId()).orElseThrow();
+                assertThat(savedUser.getEmail()).isEqualTo("test-integration-app@gmail.com");
+                assertThat(savedUser.getNickname()).isEqualTo("my-nickname");
+        }
 
-        // when
-        Integer userId = userService.register(dto);
+        @Test
+        @DisplayName("회원가입 성공 - 닉네임이 비어있어 자동 생성될 때")
+        void register_success_auto_nickname() {
+                // given
+                UserRegisterDto dto = UserRegisterDto.builder()
+                                .email("auto-nick-app@naver.com")
+                                .password("Password123+")
+                                .nickname("")
+                                .viewLangCode("ko")
+                                .nativeLangCode("en")
+                                .build();
 
-        // then
-        User savedUser = userRepository.findById(userId).orElseThrow();
-        assertThat(savedUser.getNickname()).isEqualTo("auto-nick-app");
-    }
+                // when
+                User user = userService.register(dto);
 
-    @Test
-    @DisplayName("회원가입 실패 - 이메일 중복")
-    void register_fail_duplicate_email() {
-        // given
-        String email = "duplicate-app@gmail.com";
-        userRepository.save(User.builder()
-                .email(email)
-                .password("Password123*")
-                .nickname("nick1")
-                .viewLangCode("ko")
-                .nativeLangCode("en")
-                .build());
+                // then
+                User savedUser = userRepository.findById(user.getUserId()).orElseThrow();
+                assertThat(savedUser.getNickname()).isEqualTo("auto-nick-app");
+        }
 
-        UserRegisterDto dto = UserRegisterDto.builder()
-                .email(email)
-                .password("Password123*")
-                .nickname("nick2")
-                .viewLangCode("ko")
-                .nativeLangCode("en")
-                .build();
+        @Test
+        @DisplayName("회원가입 실패 - 이메일 중복")
+        void register_fail_duplicate_email() {
+                // given
+                String email = "duplicate-app@gmail.com";
+                userRepository.save(User.builder()
+                                .email(email)
+                                .password("Password123*")
+                                .nickname("nick1")
+                                .viewLangCode("ko")
+                                .nativeLangCode("en")
+                                .build());
 
-        // when & then
-        assertThatThrownBy(() -> userService.register(dto))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("이미 이메일이 존재합니다");
-    }
+                UserRegisterDto dto = UserRegisterDto.builder()
+                                .email(email)
+                                .password("Password123*")
+                                .nickname("nick2")
+                                .viewLangCode("ko")
+                                .nativeLangCode("en")
+                                .build();
 
-    @Test
-    @DisplayName("로그인 성공")
-    void login_flow_success() {
-        // given
-        String email = "login@test.com";
-        String password = "Password123*";
-        userRepository.save(User.builder()
-                .email(email)
-                .password(password)
-                .viewLangCode("ko")
-                .nativeLangCode("en")
-                .build());
+                // when & then
+                assertThatThrownBy(() -> userService.register(dto))
+                                .isInstanceOf(IllegalArgumentException.class)
+                                .hasMessageContaining("이미 이메일이 존재합니다");
+        }
 
-        UserLoginDto dto = UserLoginDto.builder()
-                .email(email)
-                .password(password)
-                .build();
+        @Test
+        @DisplayName("로그인 성공")
+        void login_flow_success() {
+                // given
+                String email = "login@test.com";
+                String password = "Password123*";
+                userRepository.save(User.builder()
+                                .email(email)
+                                .password(passwordEncoder.encode(password))
+                                .viewLangCode("ko")
+                                .nativeLangCode("en")
+                                .build());
 
-        // when
-        Integer userId = userService.login(dto);
+                UserLoginDto dto = UserLoginDto.builder()
+                                .email(email)
+                                .password(password)
+                                .build();
 
-        // then
-        assertThat(userId).isNotNull();
-    }
+                // when
+                User user = userService.authenticate(dto.getEmail(), dto.getPassword());
 
-    @Test
-    @DisplayName("로그인 실패 - 잘못된 비밀번호")
-    void login_flow_fail_wrong_password() {
-        // given
-        String email = "login-fail@test.com";
-        userRepository.save(User.builder()
-                .email(email)
-                .password("correct-pass123")
-                .viewLangCode("ko")
-                .nativeLangCode("en")
-                .build());
+                // then
+                assertThat(user.getUserId()).isNotNull();
+        }
 
-        UserLoginDto dto = UserLoginDto.builder()
-                .email(email)
-                .password("wrong-pass123")
-                .build();
+        @Test
+        @DisplayName("로그인 실패 - 잘못된 비밀번호")
+        void login_flow_fail_wrong_password() {
+                // given
+                String email = "login-fail@test.com";
+                userRepository.save(User.builder()
+                                .email(email)
+                                .password(passwordEncoder.encode("correct-pass123"))
+                                .viewLangCode("ko")
+                                .nativeLangCode("en")
+                                .build());
 
-        // when & then
-        assertThatThrownBy(() -> userService.login(dto))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("이메일 또는 비밀번호가 올바르지 않습니다");
-    }
+                UserLoginDto dto = UserLoginDto.builder()
+                                .email(email)
+                                .password("wrong-pass123")
+                                .build();
+
+                // when & then
+                assertThatThrownBy(() -> userService.authenticate(dto.getEmail(), dto.getPassword()))
+                                .isInstanceOf(IllegalArgumentException.class)
+                                .hasMessageContaining("이메일 또는 비밀번호가 올바르지 않습니다");
+        }
 }
