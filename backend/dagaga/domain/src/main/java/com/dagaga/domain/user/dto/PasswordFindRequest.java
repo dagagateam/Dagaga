c4a5dfcbf163d4d6339086801587cc807a3ba4e9
Arin Kim <arin.kim0801@gmail.com>
2026-02-05 11:18:39 +0900

    Feat: 비밀번호 찾기 추가

--- backend/dagaga/domain/src/main/java/com/dagaga/domain/user/dto/PasswordFindRequest.java
+++ backend/dagaga/domain/src/main/java/com/dagaga/domain/user/dto/PasswordFindRequest.java
@@ -1,15 +1,15 @@
 package com.dagaga.domain.user.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class PasswordFindRequest {
-    
+
     @NotBlank(message = "이메일은 필수 입력값입니다.")
-    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    @Pattern(regexp = "^[A-Za-z0-9.]+@[A-Za-z0-9.]+\\.[A-Za-z0-9.]+$", message = "올바른 이메일 형식이 아닙니다.")
     private String email;
 }
