7ca80d6a0bc935f4e8bedd4ace65cb1abaf18002
sseooh <sseooh@gmail.com>
2026-02-06 10:49:39 +0900

    Perf: 댓글 목록 조회 DB 부하 개선

--- backend/dagaga/domain/src/main/java/com/dagaga/domain/post/repository/CommentRepository.java
+++ backend/dagaga/domain/src/main/java/com/dagaga/domain/post/repository/CommentRepository.java
@@ -2,10 +2,12 @@ package com.dagaga.domain.post.repository;
 
 import com.dagaga.domain.post.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    @EntityGraph(attributePaths = "translations")
     List<Comment> findAllByPostIdOrderByCreatedAtAsc(Integer postId);
 
     List<Comment> findAllByParentCommentIdOrderByCreatedAtAsc(Integer parentCommentId);
